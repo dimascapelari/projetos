1e13685b775d6280e2045fec9744ab1f407b2b25
Dimas Capelari <97951537+DimasCapelari@users.noreply.github.com>
2022-03-31 21:47:44 -0300

    atualizações

--- netflix-master/scripts.js
+++ netflix-master/scripts.js
@@ -1,3 +1,13 @@
 let botao = document.getElementById('botao-pergunta')
+let menu = document.getElementById('pergunta')
 
-botao.addEventListener('click',)
+function abacate() {
+
+  if (menu.style.display == 'none') {
+    menu.style.display = 'block'
+  } else {
+    menu.style.display = 'none'
+  }
+}
+
+botao.addEventListener('click', abacate)
