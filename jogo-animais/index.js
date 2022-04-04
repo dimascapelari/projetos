4e8829d983cdc9051339a97ab0116f1b58dfbb1c
Dimas Capelari <97951537+DimasCapelari@users.noreply.github.com>
2022-04-04 16:27:08 -0300

    atualiza

--- jogo-animais/index.js
+++ jogo-animais/index.js
@@ -1,16 +1,11 @@
-function verificar() {
-  let tartaruga = document.getElementById('tartaruga')
-  let hipopotamo = document.getElementById('hipopotamo')
-  let macaco = document.getElementById('macaco')
-  let guepardo = document.getElementById('guepardo')
+function verificar1() {
   var animal = document.getElementsByName('radanimal')
   var res = document.getElementById('res')
   var img = document.createElement('img')
 
   if (animal[3].checked) {
     res.innerHTML = 'Você acertou! <br>'
-    res.style.color = "blue";
-    //img.setAttribute('id', 'foto')
+    res.style.color = "blue"
     img.setAttribute('src', './img/guepardo.png')
     document.body.style.background = 'green'
   } else if (animal[0].checked) {
@@ -36,3 +31,43 @@ function verificar() {
   res.style.textAlign = 'center'
   res.appendChild(img)
 }
+
+
+
+
+function verificar2() {
+  var animal = document.getElementsByName('radanimal')
+  var res = document.getElementById('res')
+  var img = document.createElement('img')
+
+  if (animal[1].checked) {
+    res.innerHTML = 'Você acertou! <br>'
+    res.style.color = "blue"
+    img.setAttribute('src', './img/girafa.png')
+    document.body.style.background = 'green'
+  } else if (animal[0].checked) {
+    res.innerHTML = 'Você errou! <br>'
+    img.setAttribute('src', './img/leao.png')
+    document.body.style.background = 'red'
+    res.style.color = "#ff0000";
+  } else if (animal[2].checked) {
+    res.innerHTML = 'Você errou! <br>'
+    img.setAttribute('src', './img/javali.png')
+    document.body.style.background = 'red'
+    res.style.color = "#ff0000";
+  } else if (animal[3].checked) {
+    res.innerHTML = 'Você errou! <br>'
+    img.setAttribute('src', './img/suricate.png')
+    document.body.style.background = 'red'
+    res.style.color = "#ff0000";
+  } else {
+    res.innerHTML = 'Escolha uma opção!'
+    res.style.color = "#ff0000";
+  }
+
+  res.style.textAlign = 'center'
+  res.appendChild(img)
+
+}
+
+
