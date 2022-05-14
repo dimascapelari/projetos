cd622566fea3ee7997eea214ffc3905d48f174f0
Dimas Capelari <dimas.capelari@gmail.com>
2022-05-14 15:00:49 -0300

    updates

--- Alura-React/projeto/src/components/Botao/index.tsx
+++ Alura-React/projeto/src/components/Botao/index.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
+import './style.scss';
 
 class Botao extends React.Component {
     render() {
-        const backgroundColor = 'orange';
         return (
-            <button style={{
-                backgroundColor
-            }}>
+            <button className='botao'>
                 Botão
             </button>
         )
