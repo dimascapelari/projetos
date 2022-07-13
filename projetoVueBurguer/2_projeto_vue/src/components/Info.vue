063548c6a9cbd593d82e5e79db95d9e1cf6b00db
Dimas Capelari <dimas.capelari@gmail.com>
2022-07-13 13:28:45 -0300

    diretivas

--- projetoVueBurguer/2_projeto_vue/src/components/Info.vue
+++ projetoVueBurguer/2_projeto_vue/src/components/Info.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <p>Estou trabalhando no momento.</p>
+    <p v-if="esta_trabalhando">Estou trabalhando no momento.</p>
+    <p v-else>Estou em busca de novas oportunidades!</p>
     <p>Utilizo as seguintes tecnologias:</p>
     <ul>
       <li>Vue.JS</li>
@@ -8,12 +9,20 @@
       <li>HTML</li>
       <li>CSS</li>
     </ul>
+    <p v-show="mostrar_email">Mande uma mensagem para: {{ email }}</p>
   </div>
 </template>
 
 <script>
 export default {
   name: "Info",
+  data() {
+    return {
+      esta_trabalhando: false,
+      mostrar_email: true,
+      email: "dimas.capelari@gmail.com",
+    };
+  },
 };
 </script>
 
