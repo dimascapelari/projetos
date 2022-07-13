0a279ed3ebfe7d18760b4b8939c8d73c5d08d134
Dimas Capelari <dimas.capelari@gmail.com>
2022-07-13 11:15:58 -0300

    Life-Cycles-Hooks

--- projetoVueBurguer/2_projeto_vue/src/App.vue
+++ projetoVueBurguer/2_projeto_vue/src/App.vue
@@ -2,16 +2,19 @@
   <div>
     <PrimeiroComponente />
     <p>O data de App é: {{ testando }}</p>
+    <LifeCycle />
   </div>
 </template>
 
 <script>
 import PrimeiroComponente from "./components/PrimeiroComponente.vue";
+import LifeCycle from "./components/LifeCycle.vue";
 
 export default {
   name: "App",
   components: {
     PrimeiroComponente,
+    LifeCycle,
   },
   data() {
     return {
---
+++ projetoVueBurguer/2_projeto_vue/src/components/LifeCycle.vue
@@ -0,0 +1,29 @@
+<template>
+  <h1>Meu nome é: {{ name }}</h1>
+</template>
+
+<script>
+export default {
+  name: "LifeCycle",
+  data() {
+    return {
+      name: "Ainda não sei",
+    };
+  },
+  // created() são os HOOKS
+  created() {
+    setTimeout(() => {
+      this.name = "Dimas";
+    }, 1000);
+  },
+  // mounted() é uma HOOK que está acima da hierarquia do created()
+  mounted() {
+    setTimeout(() => {
+      this.name = "Dimas Capelari";
+    }, 2000);
+  },
+};
+</script>
+
+<style>
+</style>
