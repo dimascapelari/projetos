a8a076342df2b1bc3fe12552161c67eab416c361
Dimas Capelari <dimas.capelari@gmail.com>
2022-07-13 10:32:26 -0300

    projetoVueBurguer

---
+++ projetoVueBurguer/2_projeto_vue/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <PrimeiroComponente />
+    <DimasComponent />
+  </div>
+</template>
+
+<script>
+import PrimeiroComponente from "./components/PrimeiroComponente.vue";
+import DimasComponent from "./components/DimasComponent.vue";
+
+export default {
+  name: "App",
+  components: {
+    PrimeiroComponente,
+    DimasComponent,
+  },
+};
+</script>
+
+<style>
+</style>
---
+++ projetoVueBurguer/2_projeto_vue/src/components/DimasComponent.vue
@@ -0,0 +1,12 @@
+<template>
+  <h2>Teste do DIMAS</h2>
+</template>
+
+<script>
+export default {
+  name: "DimasComponent",
+};
+</script>
+
+<style>
+</style>
