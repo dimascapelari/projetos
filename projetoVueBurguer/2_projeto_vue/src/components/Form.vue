6394d93907d497efb62c2deb1658afec846d74ef
Dimas Capelari <dimas.capelari@gmail.com>
2022-07-13 16:55:16 -0300

    eventos

--- projetoVueBurguer/2_projeto_vue/src/components/Form.vue
+++ projetoVueBurguer/2_projeto_vue/src/components/Form.vue
@@ -1,11 +1,14 @@
 <template>
   <div>
     <h2>Meu formulário:</h2>
-    <form action="">
-      <div><InputText /></div>
-      <div><InputText /></div>
+    <form action="" @submit="enviarFormulario($event)">
+      <div><input type="text" v-model="name" /></div>
+      <div><input type="text" v-model="email" /></div>
       <div><Submit /></div>
     </form>
+    <p>Conteúdo</p>
+    <p>Conteúdo</p>
+    <p>Conteúdo</p>
   </div>
 </template>
 
@@ -19,8 +22,29 @@ export default {
     InputText,
     Submit,
   },
+  data() {
+    return {
+      name: "",
+      email: "",
+    };
+  },
+  methods: {
+    enviarFormulario(e) {
+      e.preventDefault();
+
+      const name = this.name;
+      const email = this.email;
+
+      console.log("Formulário Enviado!");
+      console.log("O nome é: " + name);
+      console.log("O email é: " + email);
+    },
+  },
 };
 </script>
 
-<style>
+<style scoped>
+p {
+  margin: 150px;
+}
 </style>
