ea562e5960a96c9ef1ddd1dbdd43645a36bf73ed
Dimas Capelari <dimas.capelari@gmail.com>
2022-07-14 14:40:38 -0300

    $emit

--- projetoVueBurguer/2_projeto_vue/src/components/Picture.vue
+++ projetoVueBurguer/2_projeto_vue/src/components/Picture.vue
@@ -1,20 +1,28 @@
 <template>
   <div>
     <img :src="avatar" :alt="descricao" />
-    <Reutilizacao />
+    <MudarImagem @mudar-imagem="trocarImagem" />
   </div>
 </template>
 
 <script>
-import Reutilizacao from "./Reutilizacao.vue";
+import MudarImagem from "./MudarImagem.vue";
+
 export default {
-  components: { Reutilizacao },
+  components: {
+    MudarImagem,
+  },
   name: "Picture",
   data() {
     return {
       avatar: "/img/avatar.png",
       descricao: "Dimas Capelari",
     };
+  },
+  methods: {
+    trocarImagem() {
+      this.avatar = "/img/avatar2.png";
+    },
   },
 };
 </script>
