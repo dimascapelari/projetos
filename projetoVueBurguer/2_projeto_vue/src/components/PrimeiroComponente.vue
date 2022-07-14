9e11dc4b530b23fdf2f417c21a70fcc1308e7415
Dimas Capelari <dimas.capelari@gmail.com>
2022-07-14 12:42:29 -0300

    multiplosEventos

--- projetoVueBurguer/2_projeto_vue/src/components/PrimeiroComponente.vue
+++ projetoVueBurguer/2_projeto_vue/src/components/PrimeiroComponente.vue
@@ -5,12 +5,19 @@
       Meu nome é {{ nome }}, tenho {{ age }} anos e trabalho como
       {{ profissao }}.
     </p>
+
+    <MultiplosEventos />
   </div>
 </template>
 
 <script>
+import MultiplosEventos from "./MultiplosEventos.vue";
+
 export default {
   name: "PrimeiroComponente",
+  components: {
+    MultiplosEventos,
+  },
   //data() são os dados que podem ser usados dentro deste componente e não fora dele
   data() {
     return {
